b438ec70745c6fe9168a03d563afcb66316d0754
techsorceror <wangleon840@gmail.com>
2023-11-14 23:26:50 +0200

    temp

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  Modal,
+  View,
+  Pressable,
+  FlatList,
+} from "react-native";
 import styled from "styled-components/native";
 import DropDownPicker from "react-native-dropdown-picker";
 import Icon from "react-native-vector-icons/Entypo";
-import {
-  Menu,
-  MenuProvider,
-  MenuOptions,
-  MenuOption,
-  MenuTrigger,
-} from "react-native-popup-menu";
 
 interface DropdownType {
   isSelected: boolean;
@@ -34,67 +34,104 @@ const Dropdown = ({
   const [items, setItems] = useState(
     data.map((item) => ({ label: item, value: item }))
   );
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const renderItem = ({ item }: any) => {
+    return (
+      <Item>
+        <Text>{item}</Text>
+      </Item>
+    );
+  };
 
   return (
     <Wrapper>
-      <MenuProvider>
-        <Menu>
-          <MenuTrigger>
-            <MenuButton>
-              <ButtonText>Click Here!</ButtonText>
-            </MenuButton>
-          </MenuTrigger>
-          <MenuOptions>
-            <MenuOption onSelect={() => {}} text="Save" />
-            <MenuOption onSelect={() => {}}>
-              <Text style={{ color: "red" }}>Delete</Text>
-            </MenuOption>
-            <MenuOption onSelect={() => {}} disabled={true} text="Disabled" />
-          </MenuOptions>
-        </Menu>
-      </MenuProvider>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <ModalWrapper>
+          <FlatList
+            data={data}
+            renderItem={renderItem}
+            keyExtractor={(item) => item}
+            style={style.Modal}
+            contentContainerStyle={[
+              style.ModalContainer,
+              { backgroundColor: primaryColor },
+            ]}
+          />
+        </ModalWrapper>
+      </Modal>
+      <MenuButton
+        android_ripple={{ color: "#ffffff30", borderless: true }}
+        onPress={() => setModalVisible(true)}
+      >
+        <ButtonText>{selectedItem}</ButtonText>
+      </MenuButton>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.View`
-  width: 60%;
-`;
-
-const MenuButton = styled.View`
-  border-width: 2px;
+  width: 80%;
+  height: 48px;
   border-radius: 24px;
   border-color: #fff;
+  border-width: 2px;
+`;
+
+const MenuButton = styled.Pressable`
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  margin: 1px;
 `;
 
 const ButtonText = styled.Text`
   color: #fff;
+  font-size: 18px;
+  font-weight: 500;
 `;
 
-const styles = StyleSheet.create({
-  dropdown: {
-    borderWidth: 2,
+const ModalContainer = styled.View<ColorType>`
+  border-radius: 24px;
+  background-color: ${(props) => props.color};
+  border: 2px solid #fff;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+`;
+
+const ModalWrapper = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Item = styled.View`
+  padding: 12px;
+  font-size: 18px;
+  width: 100%;
+  color: #fff;
+  /* border: 1px solid #fff; */
+`;
+
+const style = StyleSheet.create({
+  Modal: {
+    width: "80%",
+    flexGrow: 0,
+    flexDirection: "column-reverse",
+  },
+  ModalContainer: {
     borderRadius: 24,
-    borderColor: "#fff",
-    color: "#fff",
-  },
-  dropdownContainer: {
     borderWidth: 2,
-    borderTopWidth: 1,
-    borderRadius: 24,
     borderColor: "#fff",
-    color: "#fff",
-  },
-  text: {
-    color: "#fff",
-    fontSize: 18,
-    fontWeight: "500",
-  },
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    marginVertical: 200,
-    marginHorizontal: 100,
+    height: "auto",
   },
 });
 
